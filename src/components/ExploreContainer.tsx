0834d7c868437730319f8468ea38ec8b492727bf
Amanda <windsoramanda@outlook.com>
2022-10-19 11:45:12 +1000

    octoberzzzz

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,53 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonIcon,
+  IonContent,
+} from "@ionic/react";
+import {
+  calendarOutline,
+  clipboardOutline,
+  stopwatchOutline,
+  trophyOutline,
+} from "ionicons/icons";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonContent fullscreen>
+      <div className="container">
+        <IonCard className="Skills" href="/skills">
+          <IonCardContent>
+            <IonCardTitle>Skills</IonCardTitle>
+            <IonIcon icon={trophyOutline}></IonIcon>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard className="Routines" href="/routines">
+          <IonCardContent>
+            <IonCardTitle>Routines</IonCardTitle>
+            <IonIcon icon={clipboardOutline}></IonIcon>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard className="Training Plan" href="/trainingPlan">
+          <IonCardContent>
+            <IonCardTitle>Training Plan</IonCardTitle>
+            <IonIcon icon={calendarOutline}></IonIcon>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard className="Workout" href="/workout">
+          <IonCardContent>
+            <IonCardTitle>Workout</IonCardTitle>
+            <IonIcon icon={stopwatchOutline}></IonIcon>
+          </IonCardContent>
+        </IonCard>
+      </div>
+    </IonContent>
   );
 };
 
